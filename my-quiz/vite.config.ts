3defccec291e21cb2f83b4c6ec9bdc5f447512cd
Julian <juliiansandstrom@gmail.com>
2024-12-12 15:58:35 +0100

    Fixed asset paths for GH pages and Netlifys

--- my-quiz/vite.config.ts
+++ my-quiz/vite.config.ts
@@ -4,9 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/my-quiz/" : "/",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets", // Specify where the assets go in the dist directory
-  },
+  base: process.env.VITE_APP_BASE_PATH || "/",
 });
