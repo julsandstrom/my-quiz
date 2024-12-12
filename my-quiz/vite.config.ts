74ad22162f6363bfe182319d264f6e193559ed34
Julian <juliiansandstrom@gmail.com>
2024-12-12 15:33:27 +0100

    Set base path for Netlify and Github Pages

--- my-quiz/vite.config.ts
+++ my-quiz/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: process.env.BASE_PATH || "/", // Use BASE_PATH from .env, fallback to root if not defined
+  build: {
+    outDir: "dist",
+    assetsDir: "assets", // Specify where the assets go in the dist directory
+  },
+});
